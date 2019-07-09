e19de00ce7e9a835c1e6758eb778ebc19281178d
Ewerton da Silva <ewertons@roxa>
2019-07-08 23:22:44 -0700

    Add stringx, library of string helpers

---
+++ inc/stringx.h
@@ -0,0 +1,8 @@
+#ifndef STRINGX_H
+#define STRINGX_H
+
+#include <stdlib.h>
+
+extern int stringx_clone(char** dst, const char* src);
+
+#endif // STRINGX_H
---
+++ src/stringx.c
@@ -0,0 +1,27 @@
+#include "stringx.h"
+
+int stringx_clone(char** dst, const char* src)
+{
+	int result;
+	
+	if (dst == NULL || src == NULL)
+	{
+		result = __LINE__;
+	}
+	else
+	{
+		int length = strlen(src) + 1;
+		
+		if ((*dst = malloc(sizeof(char) * length)) == NULL)
+		{
+			result = __LINE__;
+		}
+		else
+		{
+			memcpy(*dst, src, length);
+			result = 0;
+		}
+	}
+	
+	return result;
+}
