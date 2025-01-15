7af2fc7aa7ce7ce023d87791c82a677bcb824a35
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-01-14 20:26:45 -0800

    Make test_socket parameterized

--- tests/scripts/test_setup.sh
+++ tests/scripts/test_setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+./cert_gen.sh create_root_and_intermediate  
+./cert_gen.sh create_client_certificate_from_intermediate client1
+./cert_gen.sh create_server_certificate_from_intermediate localhost
+
+sed -i "s@#define CLIENT_CERT_PATH \".*\"@#define CLIENT_CERT_PATH \"$(pwd)/certs/client.cert.pem\"@g" ../src/test_socket.c
+sed -i "s@#define CLIENT_PK_PATH \".*\"@#define CLIENT_PK_PATH \"$(pwd)/private/client.key.pem\"@g" ../src/test_socket.c
+sed -i "s@#define SERVER_CERT_PATH \".*\"@#define SERVER_CERT_PATH \"$(pwd)/certs/server.cert.pem\"@g" ../src/test_socket.c
+sed -i "s@#define SERVER_PK_PATH \".*\"@#define SERVER_PK_PATH \"$(pwd)/private/server.key.pem\"@g" ../src/test_socket.c
+sed -i "s@#define CA_CHAIN_PATH \".*\"@#define CA_CHAIN_PATH \"$(pwd)/certs/chain.ca.cert.pem\"@g" ../src/test_socket.c
--- tests/src/test_socket.c
+++ tests/src/test_socket.c
@@ -9,22 +9,31 @@
 
 #include <unistd.h> 
 
+#define CLIENT_CERT_PATH "TBD"
+#define CLIENT_PK_PATH "TBD"
+#define SERVER_CERT_PATH "TBD"
+#define SERVER_PK_PATH "TBD"
+#define CA_CHAIN_PATH "TBD"
+
 static void socket_client_and_server_success(void** state)
 {
     (void)state;
     int port = 5577;
+    uint8_t raw_write_buffer[10];
+    uint8_t raw_read_buffer[10];
+    span_t read_buffer = span_from_memory(raw_read_buffer);
 
     socket_config_t client_socket_config = socket_get_default_secure_client_config();
     client_socket_config.remote.hostname = span_from_str_literal("localhost");
     client_socket_config.remote.port = port;
-    client_socket_config.tls.certificate_file = "/home/ewertons/code/s1/azure-iot-sdk-c/tools/CACertificates/certs/client.cert.pem";
-    client_socket_config.tls.private_key_file = "/home/ewertons/code/s1/azure-iot-sdk-c/tools/CACertificates/private/client.key.pem";
-    client_socket_config.tls.trusted_certificate_file = "/home/ewertons/code/s1/azure-iot-sdk-c/tools/CACertificates/certs/chain.ca.cert.pem";
+    client_socket_config.tls.certificate_file = CLIENT_CERT_PATH;
+    client_socket_config.tls.private_key_file = CLIENT_PK_PATH;
+    client_socket_config.tls.trusted_certificate_file = CA_CHAIN_PATH;
 
     socket_config_t server_socket_config = socket_get_default_secure_server_config();
     server_socket_config.local.port = port;
-    server_socket_config.tls.certificate_file = "/home/ewertons/code/s1/azure-iot-sdk-c/tools/CACertificates/certs/server.cert.pem";
-    server_socket_config.tls.private_key_file = "/home/ewertons/code/s1/azure-iot-sdk-c/tools/CACertificates/private/server.key.pem";
+    server_socket_config.tls.certificate_file = SERVER_CERT_PATH;
+    server_socket_config.tls.private_key_file = SERVER_PK_PATH;
 
     socket_t client_socket;
     socket_t server_listen_socket;
@@ -48,7 +57,15 @@ static void socket_client_and_server_success(void** state)
 
     for (int i = 0; i < 10; i++)
     {
-      assert_int_equal(socket_write(&client_socket, span_from_str_literal("BLA!")), ok);
+      span_t bytes_read;
+      span_t write_buffer = span_from_memory(raw_write_buffer);
+      write_buffer = span_copy_int32(write_buffer, i, NULL);
+
+      assert_int_not_equal(0, span_get_size(write_buffer));
+      assert_int_equal(socket_write(&client_socket, write_buffer), ok);
+      assert_int_equal(socket_read(&server_socket, read_buffer, &bytes_read), ok);
+      assert_int_equal(span_get_size(bytes_read), span_get_size(write_buffer));
+      assert_memory_equal(span_get_ptr(bytes_read), span_get_ptr(write_buffer), span_get_size(bytes_read));
     }
 
     assert_int_equal(socket_deinit(&client_socket), ok);
