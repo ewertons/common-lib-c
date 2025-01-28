7501f1b926e5300bf2556deedd921b0d0971c342
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-01-27 17:27:27 -0800

    Fix list of headers in common_lib_c.h

--- inc/common_lib_c.h
+++ inc/common_lib_c.h
@@ -1,9 +1,18 @@
 #ifndef COMMON_LIB_C
 #define COMMON_LIB_C
 
+#include "ansi_colors.h"
+#include "base64.h"
+#include "bst_redblack.h"
 #include "circular_list.h"
+#include "list.h"
+#include "hmac_sha256.h"
 #include "niceties.h"
+#include "socket.h"
 #include "span.h"
+#include "stack.h"
+#include "stream.h"
+#include "socket_stream.h"
 #include "stringx.h"
 
 #endif // COMMON_LIB_C
